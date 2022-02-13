02b5e3f3d5de715e261d402d63026ffc27e40f37
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-02-13 23:01:03 +0100

    Add more poems

--- hafiz/dl.sh
+++ hafiz/dl.sh
@@ -2,9 +2,11 @@
 
 set -Eeuo pipefail
 
-for i in {51..100}; do
+for i in {101..200}; do
 	curl -fs "https://ganjoor.net/hafez/ghazal/sh$i/" |
 		pup ".b" |
 		pandoc -f html -t plain >"$i.txt"
 	sleep 3
 done
+
+sd "\n\n" "\n" ./*.txt
